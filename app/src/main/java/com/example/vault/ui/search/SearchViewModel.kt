664ae7ec00d574ead0dfa31ee4dfa4099b5099f1
John Howe <94157812+johnh-otu@users.noreply.github.com>
2024-10-30 16:37:30 -0400

    refactor: change "Notifications" to "Profile" and "Dashboard" to "Search"

--- app/src/main/java/com/example/vault/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/vault/ui/search/SearchViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.vault.ui.notifications
+package com.example.vault.ui.search
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class SearchViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
