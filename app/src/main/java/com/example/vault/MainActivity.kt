a3ed16bc3d98c2369edbb93d0f1011bbcca112d8
Johvonne <95516054+JvKeane@users.noreply.github.com>
2024-11-01 22:41:13 -0400

    -updated profile fragement
    updated profile fragment and deleted the activity that was there before.

--- app/src/main/java/com/example/vault/MainActivity.kt
+++ app/src/main/java/com/example/vault/MainActivity.kt
@@ -21,8 +21,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        setSupportActionBar(binding.toolbar)
 
+        val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
