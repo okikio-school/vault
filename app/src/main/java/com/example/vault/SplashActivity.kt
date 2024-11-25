9b29942ffc88e28b47da2616c30ba22bea580da9
Daniel Amas <amasdaniel3@gmail.com>
2024-11-25 15:48:27 -0500

    fixed splash screen and intro activity as well as routing

---
+++ app/src/main/java/com/example/vault/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.example.vault
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Delay and navigate
+        Handler(Looper.getMainLooper()).postDelayed({
+            val sharedPreferences = getSharedPreferences("AppPrefs", MODE_PRIVATE)
+            val isFirstRun = sharedPreferences.getBoolean("isFirstRun", true)
+
+            if (isFirstRun) {
+                startActivity(Intent(this, IntroActivity::class.java))
+            } else {
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+            finish()
+        }, 3000) // 2-second delay
+    }
+}
