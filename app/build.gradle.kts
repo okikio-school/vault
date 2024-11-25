10a25f769db833d16ae0190542986d29621d9ea2
John Howe <94157812+johnh-otu@users.noreply.github.com>
2024-11-25 14:40:33 -0500

    feat: add decryption button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.browser)
+    implementation(libs.androidx.biometric.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
